ce4e2143bd54b7c6a6d17d852828015fafc26f64
tuan2linh <champihycoql@gmail.com>
2025-05-25 14:27:28 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 connectDB();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+});
